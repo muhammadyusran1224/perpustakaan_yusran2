a3a5350dddc3c67cd0a02ead6741b2af8a87a0bb
muhammadyusran1224 <96140369+muhammadyusran1224@users.noreply.github.com>
2022-01-21 16:55:57 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,17 @@
+<?php
+
+$server   = "localhost";
+$username = "root";
+$password = "";
+$database = "yusran_perpus";
+
+$konek = mysqli_connect($server,$username,$password,$database);
+
+if (!$konek) {
+    die("KONEKSI GAGAL <br>".mysqli_connect_error()."<br>".mysqli_connect_errno());
+}
+// else{
+//     echo "KONEKSI BERHASIL";
+// }
+
+?>
